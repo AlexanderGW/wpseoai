7fa9deca21537c98d8c69c87c68bbf421cdad75b
alexandergw <alexgw@gmail.com>
2024-04-20 12:12:20 +0100

    Fix `_manually_load_plugin` path on test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -36,7 +36,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wpseoai.php';
+	require dirname( dirname( __FILE__ ) ) . '/ai-seo-wp.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
